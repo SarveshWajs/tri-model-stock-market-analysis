b666fff9c7ea1d00db3a880048c9967d034263ba
SarveshWajs <84698562+SarveshWajs@users.noreply.github.com>
2024-09-21 06:43:03 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,8 +92,8 @@ if task == "Prediction":
     
     # Make a prediction
     predicted_price_scaled = lin_reg_model.predict(X_new)
-    st.write(predicted_price_scaled)
-    predicted_price = close_scaler.inverse_transform(predicted_price_scaled)
+    st.write(predicted_price_scaled.reshape(-1, 1))
+    predicted_price = close_scaler.inverse_transform(predicted_price_scaled.reshape(-1, 1))
     st.write(f"Predicted closing price for tomorrow: ${predicted_price[0]:.2f}")
 
 # Task 2: Classification - Price Movement Prediction
